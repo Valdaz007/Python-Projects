fe207d567fb4a6f4fe16e0321c4713cbd70ee5c1
Victor <volsavaivictor@gmail.com>
2026-02-01 11:56:34 +1000

    Update: Add Counting Func

--- tkinter-learn/CountApp/app.py
+++ tkinter-learn/CountApp/app.py
@@ -1,14 +1,22 @@
 import tkinter as tk
 
+
+def btnCounter():
+    counter.set(counter.get()+1)
+
 wdw = tk.Tk()
 wdw.title('MiCounterApp')
 wdw.geometry('450x300')
 wdw.config(bg='#2b2b2b')
 
-lbl_counter = tk.Label(wdw, text='0', bg='#2b2b2b', fg='#ffffff')
+counter = tk.IntVar()
+counter.set(0)
+
+lbl_counter = tk.Label(wdw, text='0', bg='#2b2b2b', fg='#ffffff', textvariable=counter)
 lbl_counter.pack()
 
-btn_counter = tk.Button(wdw, text='Count', bg='#ffa002')
+btn_counter = tk.Button(wdw, text='Count', bg='#ffa002',
+                        command=btnCounter)
 btn_counter.pack()
 
 wdw.mainloop()
