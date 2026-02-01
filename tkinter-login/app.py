c9fa4be84aee0176928b45b5b99c498552958384
Victor <volsavaivictor@gmail.com>
2026-02-01 11:05:08 +1000

    Update: Login Validation

--- tkinter-login/app.py
+++ tkinter-login/app.py
@@ -1,24 +1,33 @@
 import tkinter as tk
 
+def btnLogin():
+    if(inp_uname.get()=='valdaz' and inp_upwd.get()=='1234'):
+        print('Login: Successful')
+    else:
+        print('Login: Invalid')
+
 wdw = tk.Tk()
 
 wdw.title('MiLogin')
 wdw.geometry('340x440')
 wdw.configure(bg="#2B2B2B")
 
+cont = tk.Frame(wdw, bg="#2b2b2b")
+cont.pack()
 
-lbl_intro = tk.Label(wdw, text='Mi Login', bg='#2b2b2b', fg="#ffa200", 
+lbl_intro = tk.Label(cont, text='Mi Login', bg='#2b2b2b', fg="#ffa200", 
                      font=('Roboto', 20))
 
-lbl_uname = tk.Label(wdw, text='Username', bg='#2b2b2b', fg='#f3f3f3',
+lbl_uname = tk.Label(cont, text='Username', bg='#2b2b2b', fg='#f3f3f3',
                      font=('Roboto', 16))
-lbl_upwd = tk.Label(wdw, text='Password', bg='#2b2b2b', fg='#f3f3f3',
+lbl_upwd = tk.Label(cont, text='Password', bg='#2b2b2b', fg='#f3f3f3',
                     font=('Roboto', 16))
 
-inp_uname = tk.Entry(wdw, font=('Roboto', 16))
-inp_upwd = tk.Entry(wdw, show="*", font=('Roboto', 16))
+inp_uname = tk.Entry(cont, font=('Roboto', 16))
+inp_upwd = tk.Entry(cont, show="*", font=('Roboto', 16))
 
-btn_login = tk.Button(wdw, text='Login', bg='#ffa200', font=('Roboto', 12))
+btn_login = tk.Button(cont, text='Login', bg='#ffa200', font=('Roboto', 12),
+                        command=btnLogin)
 
 # Placing Widgets
 lbl_intro.grid(row=0, column=0, columnspan=2, pady=10)
